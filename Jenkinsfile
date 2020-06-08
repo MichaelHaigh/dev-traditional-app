fcfdebf8f4f1356eb755a3b15c00a9b288eb51d1
Michael Haigh <mahaigh@gmail.com>
2020-06-08 15:52:38 -0400

    Updated banner, changed Jenkinsfile

--- Jenkinsfile
+++ Jenkinsfile
@@ -4,9 +4,6 @@ node("docker") {
         git url: "https://github.com/MichaelHaigh/dev-traditional-app/", credentialsId: 'aea9e704-c6d6-4aae-91be-7243a5e7e850'
         env.GIT_COMMIT = sh(script: "git rev-parse HEAD", returnStdout: true).trim()
 
-        stage "Calm: Update Traditional App"
-        step([$class: 'RunApplicationAction', actionName: 'UpdateApp', applicationName: 'dev-traditional-app', runtimeVariables: '{}'])
-
         stage "Docker Build"
         def devdockerapp = docker.build "michaelatnutanix/dev-docker-app"
 
@@ -15,7 +12,7 @@ node("docker") {
         devdockerapp.push "${env.GIT_COMMIT}"
 
         stage "Calm: Update Hybrid App"
-        step([$class: 'RunApplicationAction', actionName: 'UpdateApp', applicationName: 'dev-hybrid-app', runtimeVariables: """{
+        step([$class: 'RunApplicationAction', actionName: 'UpdateApp', applicationName: 'Hybrid_DSL_App', runtimeVariables: """{
             "label": "${env.GIT_COMMIT}"
         }"""])
     }
--- app.py
+++ app.py
@@ -7,48 +7,60 @@ import subprocess
 app = Flask(__name__)
 
 POSTGRES = {
-    'user': 'postgres',
-    'pw': 'password',
-    'db': 'my_database',
-    'host': 'localhost',
-    'port': '5432',
+    "user": "postgres",
+    "pw": "password",
+    "db": "my_database",
+    "host": "localhost",
+    "port": "5432",
 }
 
-app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+app.config["DEBUG"] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    "postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s" % POSTGRES
+)
 db.init_app(app)
 
+
 def is_docker():
-    path = '/proc/self/cgroup'
+    path = "/proc/self/cgroup"
     return (
-        os.path.exists('/.dockerenv') or
-        os.path.isfile(path) and any('docker' in line for line in open(path))
+        os.path.exists("/.dockerenv")
+        or os.path.isfile(path)
+        and any("docker" in line for line in open(path))
     )
+
 
 def is_aws():
     command = ["sudo", "dmidecode"]
-    return (any('Xen' in line for line in subprocess.check_output(command).split('\n')))
+    return any("Xen" in line for line in subprocess.check_output(command).split("\n"))
+
 
 @app.route("/")
 def main():
-    message = "Hello John Deere!"
+    message = "Hello Forrester!"
 
     if is_docker():
         runningon = "karbon.png"
-        builtby   = "jenkins.png"
+        builtby = "jenkins.png"
 
     elif is_aws():
         runningon = "amazon.png"
-        builtby   = "calm.png"
+        builtby = "calm.png"
 
     else:
         runningon = "ahv.png"
-        builtby   = "calm.png"
+        builtby = "calm.png"
 
     hostname = os.uname()[1]
 
-    return render_template('index.html', message=message, hostname=hostname,
-                           runningon=runningon, builtby=builtby)
+    return render_template(
+        "index.html",
+        message=message,
+        hostname=hostname,
+        runningon=runningon,
+        builtby=builtby,
+    )
 
-if __name__ == '__main__':
-    app.run(host = '0.0.0.0')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
+
